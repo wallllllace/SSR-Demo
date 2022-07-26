95e724ff6174bb46c08f13ce539e663b89c6aa53
wangxiaorui19 <wangxiaorui19@jd.com>
2022-07-26 10:48:45 +0800

    commit

--- config/replaceHtmlTemplateWebpackPlugin.js
+++ config/replaceHtmlTemplateWebpackPlugin.js
@@ -6,13 +6,12 @@ class SSRCompileDonePlugin {
   apply(compiler) {
     compiler.hooks.afterEmit.tap('SSRCompileDone', (stats) => {
       const htmlFilePath = path.join(__dirname, '..', 'dist', 'index.html');
-      const ssrBuildFile = path.join(__dirname, '..', 'dist', 'app.bundle.js');
       const serverBuildFile = path.join(__dirname, '..', 'dist', 'server.js');
-      const bundle = fs.readFileSync(ssrBuildFile, 'utf-8');
+      const bundle = fs.readFileSync(serverBuildFile, 'utf-8');
       const html = fs.readFileSync(htmlFilePath, 'utf-8');
       const minifedHtml = minify(html, { collapseWhitespace: true, quoteCharacter: '\'' });
       const newBundle = bundle.replace(/__SERVER_HTML_TEMPLATE__/, minifedHtml);
-      fs.writeFileSync(ssrBuildFile, newBundle, 'utf-8');
+      fs.writeFileSync(serverBuildFile, newBundle, 'utf-8');
     });
   }
 }
--- src/server/App.js
+++ src/server/App.js
@@ -7,23 +7,23 @@ const cheerio = require('cheerio');
 const app = express();
 app.get('/', function(req,res){
     const content = renderToString(<Home/>);
-    const $ = cheerio.load('__SERVER_HTML_TEMPLATE__');
-    $('root').append(content);
-    const html = $.html();
-    res.send(html);
+    // const c = cheerio.load("__SERVER_HTML_TEMPLATE__");
+    // c('root').append('<body>Hello World ${content} !</body>');
+    // const html = c.html();
+    // res.send(html);
     
-    // res.send(`
-    //     <html>
-    //         <head>
-    //             <title> SSR demo </title>
-    //             <link rel="stylesheet" type="text/css" href="./app.css">
-    //             <script src="./app.bundle.js"></script>
-    //         </head>
-    //         <body>
-    //             Hello World ${content} !
-    //         </body>
-    //     </html>
-    // `);
+    res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="UTF-8">
+        <title>SSR Demo</title>
+      <script defer src="app.bundle.js"></script></head>
+      <body>
+        <div id="root">${content}</div>
+      </body>
+    </html>
+    `);
 });
 
 app.listen(3000, function(){
